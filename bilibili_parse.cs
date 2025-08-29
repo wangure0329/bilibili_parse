fc4dfd7df587e2bd99010f8d62702f31520bf0bf
婉糕 <90932767+wangure0329@users.noreply.github.com>
2025-08-29 10:01:28 +0900

    Add files via upload

---
+++ bilibili_parse.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class bilibili_parse : UdonSharpBehaviour {
+    public InputField Input;
+    public InputField Output;
+
+    public void ConvertUrl() {
+        if (Input.text != string.Empty) Output.text = "https://jx.91vrchat.com/bl/?url=" + Input.text;
+        Debug.Log(Input.text);
+    }
+    
+    public void ClearUrls() {
+        Input.text = string.Empty;
+        Output.text = string.Empty;
+    }
+}
